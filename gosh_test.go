b8c837533d0863a63279c03a1096acf8d8ed5f82
hai <whaibin01@126.com>
2016-04-17 14:19:04 +0800

    update test case.

--- gosh_test.go
+++ gosh_test.go
@@ -3,9 +3,6 @@ package gosh
 import "testing"
 
 func TestGosh(t *testing.T) {
-	EchoTime = true
-	EchoIn = true
-	EchoOut = true
 	err := Run("date")
 	if err != nil {
 		t.Error("gosh run error.")
